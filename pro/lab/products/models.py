7114804fed4cc2ae9024fcaa20a1f7e7433514ec
shahd mohammed <shhdmm932@gmail.com>
2025-09-07 05:51:35 +0300

    ..

---
+++ pro/lab/products/models.py
@@ -0,0 +1,10 @@
+from django.db import models # type: ignore
+
+class books(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to="book/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
